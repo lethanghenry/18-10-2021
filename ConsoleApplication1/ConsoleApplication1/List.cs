350c81a1aa094f6b734108f4ce8b38b2a1b1879a
Lê Văn Thắng <87084962+lethanghenry@users.noreply.github.com>
2021-10-18 16:07:22 +0700

    Lê Văn Thắng. Buổi 1 ngày 18-10-2021

---
+++ ConsoleApplication1/ConsoleApplication1/List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class List
+    {
+        static List<Product> listProduct = new List<Product>();
+        static void AddProduct(Product p)
+        {
+            listProduct.Add(p);
+        }
+        static void Display()
+        {
+            foreach(Product p in listProduct)
+            {
+                Console.WriteLine(p);
+            }
+           
+        }
+        static int find1( string nameProduct)
+        {
+            Product p = new Product();
+            p.name = nameProduct;
+            
+            return listProduct.IndexOf(p);
+        }
+        static void findProduct(List<Product> listProduct, string nameProduct)
+        {
+            Console.WriteLine("nhập name product cần tìm: ");
+            nameProduct = Console.ReadLine();
+            if (find1(nameProduct) == -1)
+                Console.WriteLine("không có name product này");
+            else
+            {
+                Console.WriteLine("Product " + listProduct[find1(nameProduct)]);
+            }
+        }
+    }
+}
